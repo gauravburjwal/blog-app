6b13ba4341b6dacd9ba1a9ec8463e5969b1aff87
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-15 14:06:25 +0530

    :sparkles: Add Home and Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <Navbar />
+            <div className='content'>
+                <Home />
+            </div>
         </div>
     );
-}
+};
 
 export default App;
