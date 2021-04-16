2bb00775e9e4bb75ef1d8c1cc1889092ee34e8b7
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-16 21:26:42 +0530

    :sparkles: Add create route

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -5,6 +5,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
             {blogs.map((blog) => (
                 <div className='blog-preview' key={blog.id}>
                     <h2>{blog.title} </h2>
+
                     <p>Written by: {blog.author} </p>
                 </div>
             ))}
