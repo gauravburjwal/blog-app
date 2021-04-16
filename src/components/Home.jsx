2f0b3f7ab2f75b98145fcef21cba83c4bc451b24
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-16 18:17:05 +0530

    :sparkles: Add fetch request from local server

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'My new website',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 1,
-        },
-        {
-            title: 'Welcome party!',
-            body: 'lorem ipsum...',
-            author: 'yoshi',
-            id: 2,
-        },
-        {
-            title: 'Web dev top tips',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 3,
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then((res) => {
+                    return res.json();
+                })
+                .then((result) => {
+                    setBlogs(result);
+                    setIsPending(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }, 1000);
+    }, []);
+
     return (
         <div className='home'>
-            <BlogList blogs={blogs} />
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs' />}
+            {blogs && (
+                <BlogList
+                    blogs={blogs.filter((blog) => blog.author === 'mario')}
+                    title="Mario's Blog"
+                />
+            )}
         </div>
     );
 };
