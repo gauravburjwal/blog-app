d296f2c40ddf6ad2931d9a53791bbaed6a5b22ac
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-17 16:03:51 +0530

    :sparkles: Add delete button to blog details page

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './../hooks/useFetch';
 
 const BlogDetails = () => {
@@ -6,6 +6,15 @@ const BlogDetails = () => {
     const { data: blog, isPending, error } = useFetch(
         `http://localhost:8000/blogs/${id}`
     );
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE',
+        }).then(() => {
+            history.push('/');
+        });
+    };
 
     return (
         <div className='blog-details'>
@@ -16,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by: {blog.author}</p>
                     <div>{blog.body} </div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
